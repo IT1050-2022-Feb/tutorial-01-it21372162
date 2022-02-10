4b4c342f3ce9613ccc82d2852769fb58ece9a5dd
dilshan <99360801+it21372162@users.noreply.github.com>
2022-02-10 09:13:11 +0000

    completed exercise 02

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,26 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+ //geting input
+  float distance;
+  
+  printf("enter the distance the van travelled :");
+  scanf("%f",&distance);
+  //calculation
+   float amout, rem;
+  if(distance<=30)
+  {
+    amout=distance*50;
+  }
+  else
+  {
+    rem=distance-30.0;
+    amout=(rem*40)+(30*50);
+  }
+
+  //output
+
+  printf("full amount is %.2f",amout);
   
   return 0;
 }
